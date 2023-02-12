d5cc0dfccfee18f5398638e08e7bf0a9e30b12ef
Yael Solan <yaelsolan5@gmail.com>
2023-02-12 03:20:49 +0200

    add group arguments

--- fibn.py
+++ fibn.py
@@ -8,13 +8,21 @@ def fib(n):
 
 def Main():
 	parser = argparse.ArgumentParser()
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument("-v", "--verbose", action="store_true")
+	group.add_argument("-q", "--quiet", action="store_true")
 	parser.add_argument("num", help="The fibonacci number you wish to calculate.", type=int)
 	parser.add_argument("-o", "--output", help="Output result to a file.", action="store_true")
 
 	args = parser.parse_args()
 
 	result = fib(args.num)
-	print("The "+str(args.num)+"the fib number is "+str(result))
+	if args.verbose:
+		print("The "+str(args.num)+"the fib number is "+str(result))
+	elif args.quiet:
+		print(result)
+	else:
+		print("Fib("+str(args.num)+") = "+str(result))
 
 	if args.output:
 		f = open("fibonacci.txt", "a")
